4fd7b87c73d6e20ba111f8605444b1162ef25504
Mark T. Holder <mtholder@gmail.com>
2022-07-07 12:29:19 -0500

    closer

--- extract_newick.py
+++ extract_newick.py
@@ -3,9 +3,16 @@ from __future__ import annotations
 
 import sys
 from collections import namedtuple
-from typing import List, Set, Tuple, Union
+from typing import List, Any, Set, Tuple, Union
 from pdfminer.high_level import extract_pages, LAParams
-from pdfminer.layout import LTChar, LTFigure, LTCurve, LTText
+from pdfminer.layout import (
+    LTChar,
+    LTFigure,
+    LTCurve,
+    LTTextLine,
+    LTTextLineHorizontal,
+    LTTextLineVertical,
+)
 from pdfminer.utils import fsplit, Point, Rect
 from math import sqrt
 from enum import IntEnum
@@ -34,8 +41,9 @@ class Direction(IntEnum):
 
 CARDINAL = (Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST)
 
+
 ################################################################################
-# Code for Pt and PointMap from Ned Batchelder
+# Code for Pt and PointMap modified from code by Ned Batchelder
 #   https://nedbatchelder.com/blog/201707/finding_fuzzy_floats.html
 # "I don’t have a formal license for most of this code. I post it here in the
 # spirit of sharing. Use it for what you will. " https://nedbatchelder.com/code
@@ -55,24 +63,60 @@ class PointMap:
     def _round(self, pt, jitter):
         return Pt(round(pt.x + jitter, ndigits=self.ROUND_DIGITS))
 
-    def __getitem__(self, pt):
-        val = self._items.get(pt)
-        if val is not None:
-            return val
-
+    def _get_impl(self, pt, default=None):
+        if not isinstance(pt, Pt):
+            pt = Pt(float(pt))
+        if pt in self._items:
+            return self._items[pt], True
         for jitter in self.JITTERS:
             pt_rnd = self._round(pt, jitter)
             pt0 = self._rounded.get(pt_rnd)
             if pt0 is not None:
-                return self._items[pt0]
+                return self._items[pt0], True
+        return default, False
 
-        raise KeyError(pt)
+    def get(self, pt: Union[int, float, Pt], default=None):
+        return self._get_impl(pt, default=default)[0]
+
+    def setdefault(self, pt: Union[int, float, Pt], default=None):
+        val, was_found = self._get_impl(pt, default=default)
+        if not was_found:
+            self.__setitem__(pt, val)
+        return val
+
+    def __getitem__(self, pt):
+        val, was_found = self._get_impl(pt)
+        if not was_found:
+            raise KeyError(pt)
+        return val
 
     def __setitem__(self, pt, val):
+        if not isinstance(pt, Pt):
+            pt = Pt(float(pt))
         self._items[pt] = val
         for jitter in self.JITTERS:
             pt_rnd = self._round(pt, jitter)
-            self._rounded[pt_rnd] = pt
+            old = self._rounded.get(pt_rnd)
+            store = True
+            if old is not None:
+                if old.x != pt.x:
+                    del self._items[old]  # rounded key clash, replace old key in items
+                else:
+                    store = False  # Already stored
+            if store:
+                self._rounded[pt_rnd] = pt
+
+    def __iter__(self):
+        return iter(self._items)
+
+    def items(self):
+        return self._items.items()
+
+    def keys(self):
+        return [i.x for i in self._items.keys()]
+
+    def values(self):
+        return self._items.values()
 
 
 ################################################################################
@@ -167,7 +211,7 @@ def calc_dist(pt1: Point, pt2: Point) -> float:
 
 class PlanarContainer(object):
     def __init__(self):
-        self.by_x = []
+        self.by_x = PointMap()
         self._all_nodes = []
 
     def iter_nodes(self):
@@ -175,14 +219,14 @@ class PlanarContainer(object):
 
     def find_closest(self, point: Point, tol: float) -> Union[None, Node]:
         ptx, pty = point
-        rows = self.find_closest_rows(ptx, tol)
+        rows = self._find_closest_rows(ptx, tol)
         if not rows:
             return None
         min_diff = tol
         min_el = None
-        for row in rows:
-            for el in row:
-                dist = calc_dist((ptx, pty), (el.x, el.y))
+        for row_x, row_map in rows:
+            for cell_y, el in row_map.items():
+                dist = calc_dist((ptx, pty), (row_x, cell_y.x))
                 if dist < min_diff:
                     min_diff = dist
                     min_el = el
@@ -190,61 +234,60 @@ class PlanarContainer(object):
 
     def find_exact(self, point: Point) -> Union[None, Node]:
         ptx, pty = point
-        row = self.find_row_exact(ptx)
-        if row is None:
+        row_map = self.find_row_exact(ptx)
+        if row_map is None:
             return None
-        for el in row:
-            if el.y == pty:
-                return el
-            if el.y > pty:
-                break
-        return None
+        return row_map.get(pty)
+        # for el in row:
+        #     if el.y == pty:
+        #         return el
+        #     if el.y > pty:
+        #         break
+        # return None
 
     def find_row_exact(self, x: float) -> Union[None, List[Node]]:
-        for row in self.by_x:
-            if row[0].x == x:
-                return row
-            if row[0].x > x:
-                break
-        return None
+        return self.by_x.get(x)
 
-    def find_closest_rows(self, x: float, tol: float) -> List[List[Node]]:
+    def _find_closest_rows(self, x: float, tol: float) -> List[List[Node]]:
         by_dist = []
-        for n, row in enumerate(self.by_x):
-            dist = abs(row[0].x - x)
+        for n, row_tup in enumerate(self.by_x.items()):
+            row_x, row_map = row_tup
+            dist = abs(row_x.x - x)
             if dist < tol:
-                by_dist.append((dist, n, row))
+                by_dist.append((dist, n, (row_x.x, row_map)))
         by_dist.sort()
         return [i[-1] for i in by_dist]
 
     def new_at(self, pt: Point) -> Node:
         ptx = pt[0]
         pty = pt[1]
-        dest_row = None
-        bef_ind = None
-        for n, row in enumerate(self.by_x):
-            if row[0].x == ptx:
-                dest_row = row
-                break
-            if row[0].x > ptx:
-                bef_ind = n
-                break
+        row_map = self.by_x.setdefault(ptx, PointMap())
+        # dest_row = None
+        # bef_ind = None
+        # for n, row in enumerate(self.by_x):
+        #     if row[0].x == ptx:
+        #         dest_row = row
+        #         break
+        #     if row[0].x > ptx:
+        #         bef_ind = n
+        #         break
         nd = Node(ptx, pty)
         self._all_nodes.append(nd)
-        if dest_row:
-            bef_y_ind = None
-            for n, el in enumerate(dest_row):
-                if el.y >= pty:
-                    bef_y_ind = n
-                    break
-            if bef_y_ind is not None:
-                dest_row.insert(bef_y_ind, nd)
-            else:
-                dest_row.append(nd)
-        elif bef_ind is not None:
-            self.by_x.insert(bef_ind, [nd])
-        else:
-            self.by_x.append([nd])
+        row_map.setdefault(pty, nd)
+        # if dest_row:
+        #     bef_y_ind = None
+        #     for n, el in enumerate(dest_row):
+        #         if el.y >= pty:
+        #             bef_y_ind = n
+        #             break
+        #     if bef_y_ind is not None:
+        #         dest_row.insert(bef_y_ind, nd)
+        #     else:
+        #         dest_row.append(nd)
+        # elif bef_ind is not None:
+        #     self.by_x.insert(bef_ind, [nd])
+        # else:
+        #     self.by_x.append([nd])
         return nd
 
 
@@ -296,7 +339,7 @@ class Forest(object):
         self.graph = graph
         self.trees = []
 
-    def interpret_as_tree(self, idx: int, text_lines: List[str]) -> PhyloTree:
+    def interpret_as_tree(self, idx: int, text_lines: List[LTTextLine]) -> PhyloTree:
         comp = self.components[idx]
         while len(self.trees) <= idx:
             self.trees.append(None)
@@ -313,7 +356,7 @@ class PhyloTree(object):
         self,
         connected_nodes: Set[Node] = None,
         forest: Forest = None,
-        text_lines: List[LTText] = None,
+        text_lines: List[LTTextLine] = None,
     ):
         self.forest = forest
         self.used_text = set()
@@ -329,6 +372,7 @@ class PhyloTree(object):
         nodes_bbox = (lx, ly, hx, hy)
         lx, ly, hx, hy = float("inf"), float("inf"), float("-inf"), float("-inf")
         th, tw = [], []
+        horiz_text, vert_text = [], []
         for ltline in text_lines:
             th.append(ltline.height)
             tw.append(ltline.width)
@@ -337,8 +381,19 @@ class PhyloTree(object):
             ly = min(ly, lbb[1])
             hx = max(hx, lbb[2])
             hy = max(hy, lbb[3])
+            if isinstance(ltline, LTTextLineHorizontal):
+                horiz_text.append(ltline)
+            else:
+                assert isinstance(ltline, LTTextLineVertical)
+                vert_text.append(ltline)
         text_bbox = (lx, ly, hx, hy)
-        # by_dir = [self._try_as_tips_to(i, int_nds, ext_nds, text_lines) for i in CARDINAL]
+        print(
+            f"nodes_bbox = {nodes_bbox} text_bbox={text_bbox} {len(horiz_text)}, {len(vert_text)}"
+        )
+        by_dir = [
+            self._try_as_tips_to(i, int_nds, ext_nds, horiz_text, vert_text)
+            for i in CARDINAL
+        ]
 
         # north, east, south, west = [], [], [], []
         # for ext in externals:
@@ -364,8 +419,58 @@ class PhyloTree(object):
         # self.used_text.update(best_blob[2])
         # self.num_tips = best_blob[3]
 
-    def _try_as_tips_to(self, tip_dir, internals, externals, text_lines):
-        pass
+    def _try_as_tips_to(
+        self,
+        tip_dir: Direction,
+        internals: List[Node],
+        externals: List[Node],
+        horiz_text: List[LTTextLine],
+        vert_text: List[LTTextLine],
+    ):
+        if tip_dir in (Direction.EAST, Direction.WEST):
+            inline_t = horiz_text
+            perpindic_t = vert_text
+            if tip_dir == Direction.EAST:
+                calc_x = lambda el: el.x0
+            else:
+                calc_x = lambda el: el.x1
+            calc_y = lambda el: (el.y0 + el.y1) / 2.0
+        else:
+            inline_t = vert_text
+            perpindic_t = horiz_text
+            if tip_dir == Direction.SOUTH:
+                calc_y = lambda el: el.y1
+            else:
+                calc_y = lambda el: el.y0
+            calc_x = lambda el: (el.x0 + el.x1) / 2.0
+        print(
+            f"Trying DIR={repr(tip_dir)} ... len(externals) = {len(externals)}, len(inline_t)={len(inline_t)}"
+        )
+        by_lab, by_ext = {}, {}
+        for label_t in inline_t:
+            loc = (calc_x(label_t), calc_y(label_t))
+            dist, ext = find_closest(loc, externals)
+            by_lab[label_t] = (ext, dist)
+            prev = by_ext.get(ext)
+            if prev is None or dist < prev[-1]:
+                by_ext[ext] = (label_t, dist)
+
+        for label_t in inline_t:
+            ext, dist = by_lab[label_t]
+            if by_ext.get(ext, [None, None])[0] is label_t:
+                print(f"{dist:6.3f} - matched - {repr(label_t.get_text())}")
+            else:
+                print(f"{dist:6.3f} - unmatched - {repr(label_t.get_text())}")
+
+
+def find_closest(loc: Point, el_list: List[Any]) -> Tuple[float, Any]:
+    dist, closest = float("inf"), None
+    for el in el_list:
+        d = calc_dist(loc, el.loc)
+        if d < dist:
+            dist = d
+            closest = el
+    return dist, closest
 
 
 def _analyze_text_and_curves(text_lines, curves):
